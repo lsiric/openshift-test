f651e61bf3081ebce1dea2cf61aa78b93acf5b75
Luka Siric <siric.luka@gmail.com>
2022-07-12 15:59:23 +0200

    sever .end added

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer((req, res)=>{
-    res.writeHead(200, {
-        "Content-type": "text/html"
-    });
-    res.send("<h1>open shift test success</h1>")
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.write("Hello World!");
+  res.end();
 });
 
-server.listen(8000, ()=>{
-    console.log("listening on port 8000");
+server.listen(8000, () => {
+  console.log("listening on port 8000");
 });
